d28ea481890b767412dd74ce0accb5c90375ff61
granter0fdreams <jlrosinski@gmail.com>
2022-10-25 16:45:59 -0500

    class 9 exercise completed

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,13 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+//        System.out.println(Divide(1,0));
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +16,40 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (String file : studentFiles.values()){
+            System.out.println(CheckFileExtension(file));
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        if (y == 0){
+            try {
+                throw new java.lang.ArithmeticException("Cannot divide by zero");
+            } catch (java.lang.ArithmeticException e){
+                e.printStackTrace();
+            }
+        }
+
+        return x/y;
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        if (fileName.endsWith(".java")){
+            return 1;
+        } else if (fileName.equals("") || fileName == null){
+            try {
+                throw new FileExtensionException("Please confirm file is a .java file type.");
+            } catch (FileExtensionException e){
+                e.printStackTrace();
+            }
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
 }
